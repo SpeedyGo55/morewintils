c2d78503f0f4d55927859c9038a692ae029e52aa
SpeedyGo55 <SpeedyGo55@proton.me>
2026-03-07 19:36:52 +0100

    Auto-commit before cleanup

--- src/bin/sponge.rs
+++ src/bin/sponge.rs
@@ -1,8 +1,13 @@
-use std::io::Read;
+use std::io::{Read, Write};
 
 // sponge(1) implementation in Rust
 fn main() {
     let mut buffer = String::new();
+    // if args contains -a 
     std::io::stdin().read_to_string(&mut buffer).expect("failed to read from stdin"); // read from stdin
+    if std::env::args().len() == 1 {
+        std::io::stdout().write_all(buffer.as_bytes()).expect("failed to write to stdout"); // write to stdout
+        return;
+    }
     std::fs::write(std::env::args().nth(1).unwrap(), buffer).unwrap(); // write to file or stdout
 }
